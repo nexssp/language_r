caf53334763b5588079f9f21287bd766c075b3e2
Marcin Polak <mapoart@gmail.com>
2020-09-11 11:13:35 +0200

    Added information about first packages installation

--- nexss.r.errors.js
+++ nexss.r.errors.js
@@ -3,4 +3,6 @@ module.exports = {
     "Maybe you want to add warn=FALSE, eg: readLines(f, n=1, warn=FALSE) ",
   "is not writable":
     "maybe you need to run?: \nsudo chown -R $USER /usr/local/lib\n",
+  "The downloaded source packages are in":
+    "R was installing packages at first run. Please run the command again.",
 };
